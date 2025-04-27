a5ee5c7df31772b58b8ab1ff8df5768234b924d4
eloiseLBC <eloise.leblanc@hotmail.com>
2025-04-27 14:52:29 +0200

    :lipstick: enlarge white div when quantity up to 1

--- pizzacap-app/src/views/PizzaDetailView/PizzaDetailView.tsx
+++ pizzacap-app/src/views/PizzaDetailView/PizzaDetailView.tsx
@@ -29,12 +29,6 @@ const PizzaDetailView: React.FC = () => {
             .catch((err) => console.error(err));
     }, [pizzaId]);
 
-    useEffect(() => {
-        if (pizza?.features) {
-            console.log('Features (values):', Object.values(pizza.features));
-        }
-    }, [pizza]);
-
     if (!pizza) return <div className="text-center mt-20">Chargement...</div>;
 
     const featureIcons: { [key: string]: string } = {
@@ -44,12 +38,13 @@ const PizzaDetailView: React.FC = () => {
     };
 
     const handleAddToCart = () => {
+        setQuantity(2);
         updateCartItem({
             name: pizza.name,
             image_url: pizza.image_url,
             price: pizza.price,
             size,
-            quantity,
+            quantity: 1,
         });
     };
 
@@ -59,23 +54,15 @@ const PizzaDetailView: React.FC = () => {
                 <div>
                     <h1 className="text-5xl font-bold mb-4">{pizza.name}</h1>
                     <h2 className="text-2xl font-semibold mb-2">Ingrédients</h2>
-                    <p className="text-gray-700 mb-6 max-w-xl leading-relaxed font-jost">
-                        {pizza.ingredients}
-                    </p>
+                    <p className="text-gray-700 mb-6 max-w-xl leading-relaxed font-jost">{pizza.ingredients}</p>
 
                     <div className="flex flex-col gap-4 mb-6">
                         <div className="inline-flex items-center gap-2 bg-[#D1B381] rounded-full px-4 py-2 text-white font-outfit shadow-sm w-fit">
                             {['S', 'M', 'L'].map((s) => (
                                 <button
                                     key={s}
-                                    onClick={() =>
-                                        setSize(s as 'S' | 'M' | 'L')
-                                    }
-                                    className={`px-4 py-1 rounded-full font-bold text-sm transition-all ${
-                                        size === s
-                                            ? 'bg-green-600 text-white shadow'
-                                            : 'bg-beige text-white'
-                                    }`}
+                                    onClick={() => setSize(s as 'S' | 'M' | 'L')}
+                                    className={`px-4 py-1 rounded-full font-bold text-sm transition-all ${size === s ? 'bg-green-600 text-white shadow' : 'bg-beige text-white'}`}
                                 >
                                     {s}
                                 </button>
@@ -89,22 +76,14 @@ const PizzaDetailView: React.FC = () => {
                         <div className="inline-flex items-center gap-6 bg-[#D1B381] rounded-full px-6 py-2 text-white font-outfit shadow-sm w-fit">
                             <div className="flex flex-col items-center">
                                 <span className="text-xs mb-1">Nutriscore</span>
-                                <span className="bg-green-600 text-white rounded-full w-6 h-6 flex items-center justify-center font-bold text-sm shadow-sm">
-                                    B
-                                </span>
+                                <span className="bg-green-600 text-white rounded-full w-6 h-6 flex items-center justify-center font-bold text-sm shadow-sm">B</span>
                             </div>
                             <div className="flex flex-col items-center">
                                 <span className="text-xs mb-1">Note</span>
                                 <div className="flex items-center gap-1">
-                                    <span className="font-semibold text-base">
-                                        5
-                                    </span>
+                                    <span className="font-semibold text-base">5</span>
                                     <div className="w-5 h-5 rounded-full bg-[#D9C299] flex items-center justify-center text-sm text-white">
-                                        <img
-                                            src={starsIcon}
-                                            alt="Étoiles"
-                                            className="w-4 h-4"
-                                        />
+                                        <img src={starsIcon} alt="Étoiles" className="w-4 h-4" />
                                     </div>
                                 </div>
                             </div>
@@ -113,77 +92,51 @@ const PizzaDetailView: React.FC = () => {
                 </div>
 
                 <div className="relative w-full max-w-[900px] top-8">
-                    {/* Badge de features */}
-                    {pizza.features &&
-                        Object.keys(pizza.features).length > 0 && (
-                            <div className="absolute top-12 right-[-70px] bg-secondary-variant rounded-full px-8 py-4 shadow-md flex gap-5 items-center z-30 min-h-[70px]">
-                                {Object.entries(pizza.features).map(
-                                    ([featureName, isEnabled], index) => {
-                                        if (!isEnabled) return null;
-                                        const normalized = featureName
-                                            .toLowerCase()
-                                            .replace(/[^a-z]/g, '');
-                                        const matchKey = normalized.includes(
-                                            'vegetarian',
-                                        )
-                                            ? 'Vegetarian'
-                                            : normalized.includes('gluten')
-                                            ? 'GlutenFree'
-                                            : normalized.includes('spicy') ||
-                                              normalized.includes('epice')
-                                            ? 'Spicy'
-                                            : normalized.includes('seafood')
-                                            ? 'Seafood'
-                                            : null;
+                    {pizza.features && Object.keys(pizza.features).length > 0 && (
+                        <div className="absolute top-12 right-[-70px] bg-secondary-variant rounded-full px-8 py-4 shadow-md flex gap-5 items-center z-30 min-h-[70px]">
+                            {Object.entries(pizza.features).map(([featureName, isEnabled], index) => {
+                                if (!isEnabled) return null;
+                                const normalized = featureName.toLowerCase().replace(/[^a-z]/g, '');
+                                const matchKey = normalized.includes('vegetarian') ? 'Vegetarian' :
+                                                 normalized.includes('gluten') ? 'GlutenFree' :
+                                                 normalized.includes('spicy') || normalized.includes('epice') ? 'Spicy' : null;
+                                return matchKey && featureIcons[matchKey] ? (
+                                    <img key={index} src={featureIcons[matchKey]} alt={matchKey} className="w-16 h-16 object-contain transition-transform duration-300 transform hover:scale-110" />
+                                ) : null;
+                            })}
+                        </div>
+                    )}
 
-                                        return matchKey &&
-                                            featureIcons[matchKey] ? (
-                                            <img
-                                                key={index}
-                                                src={featureIcons[matchKey]}
-                                                alt={matchKey}
-                                                className="w-16 h-16 object-contain transition-transform duration-300 transform hover:scale-110"
-                                            />
-                                        ) : null;
-                                    },
-                                )}
-                            </div>
-                        )}
+                    <div className="relative">
+                        <div className={`absolute bottom-0 left-0 ${quantity > 1 ? 'w-[220px]' : 'w-[160px]'} h-[130px] bg-white z-30 rounded-br-[30px] transition-all duration-300`}></div>
+                        <img
+                            src={pizza.image_url}
+                            alt={pizza.name}
+                            className="object-cover rounded-[30px] z-10"
+                            style={{ height: '456px', width: '900px' }}
+                        />
+                    </div>
 
-                    {/* Image de la pizza */}
-                    <img
-                        src={pizza.image_url}
-                        alt={pizza.name}
-                        className="object-cover rounded-[30px]"
-                        style={{ height: '456px', width: '900px' }}
-                    />
-
-                    {/* Forme blanche arrondie en bas */}
-                    <div
-                        className="absolute bottom-0 left-0 w-[160px] h-[130px] bg-white z-10"
-                        style={{
-                            clipPath: 'url(#pizzaShape)',
-                            borderTopRightRadius: '30px',
-                        }}
-                    ></div>
-
-                    {/* Bouton + */}
-                    <button
-                        onClick={handleAddToCart}
-                        className="absolute bottom-[25px] left-[35px] w-20 h-20 bg-green-600 text-white rounded-full z-20 shadow text-5xl flex items-center justify-center transition-transform duration-300 transform hover:scale-105 "
-                    >
-                        +
-                    </button>
+                    {quantity > 1 ? (
+                        <div className="absolute bottom-[25px] left-[35px] bg-green-600 text-white rounded-full z-40 shadow flex items-center justify-center gap-4 px-6 py-3 text-3xl transition-all duration-300">
+                            <button onClick={() => setQuantity(quantity > 1 ? quantity - 1 : 1)} className="text-4xl font-bold">-</button>
+                            <span className="text-3xl font-bold">{quantity}</span>
+                            <button onClick={() => setQuantity(quantity + 1)} className="text-4xl font-bold">+</button>
+                        </div>
+                    ) : (
+                        <button
+                            onClick={handleAddToCart}
+                            className="absolute bottom-[25px] left-[35px] w-20 h-20 bg-green-600 text-white rounded-full z-40 shadow text-3xl flex items-center justify-center transition-transform duration-300 transform hover:scale-105"
+                        >
+                            +
+                        </button>
+                    )}
                 </div>
             </div>
 
             <div className="flex justify-center mt-6 gap-4">
-                <span className="bg-green-100 text-green-800 font-semibold px-4 py-2 rounded-full text-sm text-center">
-                    -30% sur la 2ᵠ
-                </span>
-                <span className="bg-green-100 text-green-800 font-semibold px-4 py-2 rounded-full text-sm text-center">
-                    3 achetées 1 offerte
-                </span>
+                <span className="bg-green-100 text-green-800 font-semibold px-4 py-2 rounded-full text-sm text-center">-30% sur la 2ῠ</span>
+                <span className="bg-green-100 text-green-800 font-semibold px-4 py-2 rounded-full text-sm text-center">3 achetées 1 offerte</span>
             </div>
         </div>
     );
