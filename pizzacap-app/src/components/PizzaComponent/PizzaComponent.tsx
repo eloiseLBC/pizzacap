f8489f31e5515c088fc67349ff1b6f447f4799c0
eloiseLBC <eloise.leblanc@hotmail.com>
2025-04-29 16:14:49 +0200

    :bug test features missing WIP2

--- pizzacap-app/src/components/PizzaComponent/PizzaComponent.tsx
+++ pizzacap-app/src/components/PizzaComponent/PizzaComponent.tsx
@@ -58,7 +58,7 @@ const PizzaComponent: React.FC<PizzaComponentProps> = ({
     };
 
     const isVegetarian = features?.Vegetarian === true;
-    console.log("pizza : ", name, " features : ", features)
+    console.log("pizza : ", name, " features : ", features, " isVegetarien : ", isVegetarian )
 
     return (
         <div className="w-72 rounded-3xl border border-primary-variant flex flex-col items-center shadow-md">
